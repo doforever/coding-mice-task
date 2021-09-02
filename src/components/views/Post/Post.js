99d16d2ceb1d31044cad0c6a447274f71dd297f7
Dorota Tomala <doforever@gmail.com>
2021-09-02 19:37:03 +0200

    Style Post view

--- src/components/views/Post/Post.js
+++ src/components/views/Post/Post.js
@@ -4,34 +4,34 @@ import clsx from 'clsx';
 import { useSelector, useDispatch } from 'react-redux';
 import { selectById, fetchAll } from '../../../redux/postsSlice';
 
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Button from '../../common/Button/Button';
 
 import styles from './Post.module.scss';
 
 const Post = () => {
-  const {id} = useParams();
+  const { id } = useParams();
   const postData = useSelector(state => selectById(state, id));
   const dispatch = useDispatch();
 
   useEffect(() => {
-    if (!postData) { 
+    if (!postData) {
       dispatch(fetchAll());
     }
   }, [postData, id, dispatch]);
-  
+
   if (!postData) return 'Loading...';
 
   return (
-    <div className={clsx(styles.root, 'container')}>
-      <div className={styles.post}>
+    <div className={clsx(styles.root, 'container row')}>
+      <div className={clsx('col-sm-12 col-md-8', styles.post)}>
         <h1>{postData.title}</h1>
         <p>{postData.body}</p>
-        <ul className={styles.actions}>
-          <li><Link to={`/post/${id}/edit`}><Button>Edit</Button></Link></li>
-          <li><Button>Delete</Button></li>
-        </ul>
-      </div>  
+      </div>
+      <ul className={clsx('col-md-4', styles.actions)}>
+        <li><Link to={`/post/${id}/edit`}><Button>Edit</Button></Link></li>
+        <li><Button>Delete</Button></li>
+      </ul>
     </div>
   );
 };
