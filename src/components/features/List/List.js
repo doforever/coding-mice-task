7368577156411124ebc8deef27b105933831cbc7
Dorota Tomala <doforever@gmail.com>
2021-09-02 14:37:59 +0200

    Style FAB

--- src/components/features/List/List.js
+++ src/components/features/List/List.js
@@ -23,7 +23,7 @@ const List = () => {
       <div className="grid-sm-1 grid-md-2 grid-lg-3 grid-xl-4">
         {posts.map(post => <PostBox key={post.id} className={styles.post} {...post}/>)}
       </div>
-      <FAB to='/post/add'>Add</FAB>
+      <FAB to='/post/add'>+</FAB>
     </div>
   );
 };
