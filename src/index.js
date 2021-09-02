c01bfb5ca0321ef489800243cccd035f67e9c0a9
Dorota Tomala <doforever@gmail.com>
2021-09-02 10:51:43 +0200

    Initialize project with cra

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import { store } from './app/store';
+import { store } from './redux/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+
+import './styles/normalize.scss';
+import './styles/global.scss';
 
 ReactDOM.render(
   <React.StrictMode>
